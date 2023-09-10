c48c68c159cc9121931ad95a94490a39be683283
jessicahattig <jessicahattig@gmail.com>
2023-09-10 12:15:07 -0700

    Add View().

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+// using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
 
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    // Notice the changes below!
+    public ActionResult Letter() { return View(); }
 
   }
 }
