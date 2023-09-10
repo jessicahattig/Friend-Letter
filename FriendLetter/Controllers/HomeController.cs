14ca32c82f48fb79180b49030cc922036a2e40d9
jessicahattig <jessicahattig@gmail.com>
2023-09-10 10:37:33 -0700

    Add controllers.

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    public string Hello() { return "Hello friend!"; }
+
+  }
+}
